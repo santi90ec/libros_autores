3f66328660cc912273f332ddab37bcb91cab4dfc
santi90ec <andres.atapuma@udla.edu.ec>
2021-07-15 03:29:14 -0500

    Final Commit

---
+++ book_authors_proj/books_authors_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models.fields.related import create_many_to_many_intermediary_model
+
+class Book (models.Model):
+    title=models.CharField(max_length=255)
+    desc=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    def __repr__(self):
+        return f"<Book object: {self.title} {{self.id}}"
+class Author(models.Model):
+    first_name=models.CharField(max_length=45)
+    last_name=models.CharField(max_length=45)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    notas=models.CharField(max_length=100,null=True)
+    books=models.ManyToManyField(Book, related_name="authors")
+    def __repr__(self):
+        return f"<Author object: {self.first_name} {self.last_name} {{self.id}}"
+# Create your models here.
